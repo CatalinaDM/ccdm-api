675eec6476a712a3ee25eb760945e580da1f1387
CatalinaDM <catidelgadomanzano0@gmail.com>
2026-02-11 17:49:45 -0600

    fix:Estructura funcional del CRUD de tareas y uso de validaciones

--- src/modules/task/task.controller.ts
+++ src/modules/task/task.controller.ts
@@ -1,4 +1,7 @@
-import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Delete, Get, Param, ParseIntPipe, Post, Put } from '@nestjs/common';
+import { HttpException, HttpStatus } from '@nestjs/common';
+import { Body } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { TaskService } from './task.service';
 
 @Controller('/api/task')
@@ -11,22 +14,32 @@ export class TaskController {
   }
   //!GET  http://localhost:3000/api/task/1
   @Get(':id')
-  public getTaskById(@Param('id') id: string): any {
-    return this.taskSvc.getTaskById(parseInt(id));
+  public getTaskById(@Param('id', ParseIntPipe) id: number): any {
+    const task = this.taskSvc.getTaskById(id);
+    if (task) return task;
+    throw new HttpException(
+      `Tarea con id ${id} no encontrada`,
+      HttpStatus.NOT_FOUND,
+    );
   }
+
   //* POST http://localhost:3000/api/task
   @Post()
-  public insertTask(task: any): any {
+  public insertTask(@Body() task: any): any {
     return this.taskSvc.insertTask(task);
   }
   //! PUT http://localhost:3000/api/task/:id
   @Put(':id')
-  public updateTask(id: number, task: any): any {
+  public updateTask(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() task: any,
+  ): any {
     return this.taskSvc.updateTask(id, task);
   }
+
   //? DELETE http://localhost:3000/api/task/:id
   @Delete(':id')
-  public deleteTask(id: number): string {
+  public deleteTask(@Param('id', ParseIntPipe) id: number) {
     return this.taskSvc.deleteTask(id);
   }
 }
